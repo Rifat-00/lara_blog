68980fefbce204747ce4f2ce4d7143e18874e746
Rifat-00 <rr.rifat22@gmail.com>
2023-06-08 17:02:31 +0600

    Initial commit

---
+++ database/migrations/2023_06_08_103945_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            //make a new table for a post
+            $table->id(); //make a new column for id
+            $table->string('title'); //make a new column for title
+            $table->date('posted_date'); //make a new column for posted_date
+            $table->string('author'); //make a new column for author
+            $table->longText('content'); //make a new column for content
+            $table->timestamps(); //make a new column for timestamps
+            
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased">
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        @if (Route::has('login'))
+            <div class="flex w-full fixed sm:top-0 sm:right-0 p-6 text-right z-10 dark:bg-gray-800">
+                @auth
+                    <div class="navigation">
+                        <a href="{{ url('/dashboard') }}"
+                            class="pl-10 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ">Home</a>
+                        <a href="{{ url('/dashboard') }}"
+                            class="pl-10 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ">Create</a>
+                        <a href="{{ url('/dashboard') }}"
+                            class="pl-10 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ">Dashboard</a>
+                    </div>
+            </div>
+            <div class="pt-20 m-5">
+                @yield('content')
+            </div>
+            @else
+                    {{-- <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ">Log in</a> --}}
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ">Register</a>
+                    @endif
+
+                </div>
+
+                <div class="login-form dark:bg-gray-800 p-10">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <!-- Email Address -->
+                        <div>
+                            <x-input-label class="dark:text-gray-300" for="email" :value="__('Email')" />
+                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                :value="old('email')" required autofocus autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mt-4">
+                            <x-input-label class="dark:text-gray-300" for="password" :value="__('Password')" />
+
+                            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                                autocomplete="current-password" />
+
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+
+                        <!-- Remember Me -->
+                        <div class="block mt-4">
+                            <label for="remember_me" class="inline-flex items-center">
+                                <input id="remember_me" type="checkbox"
+                                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                                    name="remember">
+                                <span class="ml-2 text-sm dark:text-gray-400">{{ __('Remember me') }}</span>
+                            </label>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                            @endif
+
+                            <x-primary-button class="ml-3 dark:bg-gray-700">
+                                {{ __('Log in') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            @endauth
+        @endif
+
+    </div>
+</body>
+
+</html>
